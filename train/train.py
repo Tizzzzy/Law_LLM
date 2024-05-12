a38753c8ec279debc75d2db45977b1c629628615
Tizzzzzzy <107573421+Tizzzzy@users.noreply.github.com>
2024-05-12 11:20:08 -0500

    Add files via upload

---
+++ train/train.py
@@ -0,0 +1,228 @@
+import argparse
+import bitsandbytes as bnb
+from datasets import load_dataset
+from functools import partial
+import os
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training, AutoPeftModelForCausalLM
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed, Trainer, TrainingArguments, BitsAndBytesConfig, \
+    DataCollatorForLanguageModeling, Trainer, TrainingArguments
+from datasets import Dataset
+import pandas as pd
+import argparse
+from huggingface_hub import login
+import json
+
+# Reproducibility
+seed = 42
+set_seed(seed)
+
+# !huggingface-cli login
+login(token = "Huggingface token")
+
+def load_model(model_name, bnb_config):
+    n_gpus = torch.cuda.device_count()
+    max_memory = f'{40960}MB'
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        quantization_config=bnb_config,
+        device_map="auto", # dispatch efficiently the model on the available ressources
+        max_memory = {i: max_memory for i in range(n_gpus)},
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=True)
+
+    tokenizer.pad_token = tokenizer.eos_token
+
+    return model, tokenizer
+
+def preprocess_batch(batch, tokenizer):
+    """
+    Tokenizing a batch
+    """
+    # print("----------------", batch.keys())
+    return tokenizer(
+        batch["Prompt"],
+        truncation=True,
+    )
+
+
+def create_bnb_config():
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16,
+    )
+
+    return bnb_config
+
+def create_peft_config(modules):
+    """
+    Create Parameter-Efficient Fine-Tuning config for your model
+    :param modules: Names of the modules to apply Lora to
+    """
+    config = LoraConfig(
+        r=16,  # dimension of the updated matrices
+        lora_alpha=64,  # parameter for scaling
+        target_modules=modules,
+        lora_dropout=0.1,  # dropout probability for layers
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+
+    return config
+
+
+def find_all_linear_names(model):
+    cls = bnb.nn.Linear4bit #if args.bits == 4 else (bnb.nn.Linear8bitLt if args.bits == 8 else torch.nn.Linear)
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module, cls):
+            names = name.split('.')
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+    if 'lm_head' in lora_module_names:  # needed for 16-bit
+        lora_module_names.remove('lm_head')
+    return list(lora_module_names)
+
+
+def print_trainable_parameters(model, use_4bit=False):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        num_params = param.numel()
+        # if using DS Zero 3 and the weights are initialized empty
+        if num_params == 0 and hasattr(param, "ds_numel"):
+            num_params = param.ds_numel
+
+        all_param += num_params
+        if param.requires_grad:
+            trainable_params += num_params
+    if use_4bit:
+        trainable_params /= 2
+    print(
+        f"all params: {all_param:,d} || trainable params: {trainable_params:,d} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+def get_max_length(model):
+    conf = model.config
+    max_length = None
+    for length_setting in ["n_positions", "max_position_embeddings", "seq_length"]:
+        max_length = getattr(model.config, length_setting, None)
+        if max_length:
+            print(f"Found max lenth: {max_length}")
+            break
+    if not max_length:
+        max_length = 1024
+        print(f"Using default max length: {max_length}")
+    return max_length
+
+def train(model, tokenizer, dataset, output_dir):
+    # Apply preprocessing to the model to prepare it by
+    # 1 - Enabling gradient checkpointing to reduce memory usage during fine-tuning
+    model.gradient_checkpointing_enable()
+
+    # 2 - Using the prepare_model_for_kbit_training method from PEFT
+    model = prepare_model_for_kbit_training(model)
+
+    # Get lora module names
+    modules = find_all_linear_names(model)
+
+    # Create PEFT config for these modules and wrap the model to PEFT
+    peft_config = create_peft_config(modules)
+    model = get_peft_model(model, peft_config)
+
+    # Print information about the percentage of trainable parameters
+    print_trainable_parameters(model)
+
+    # Training parameters
+    trainer = Trainer(
+        model=model,
+        train_dataset=dataset,
+        args=TrainingArguments(
+            per_device_train_batch_size=1,
+            gradient_accumulation_steps=4,
+            warmup_steps=2,
+            # max_steps=1000,
+            num_train_epochs=1,
+            learning_rate=2e-4,
+            fp16=True,
+            logging_steps=1,
+            output_dir="outputs",
+            optim="paged_adamw_8bit",
+        ),
+        data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False)
+    )
+
+    model.config.use_cache = False  # re-enable for inference to speed up predictions for similar inputs
+
+
+    dtypes = {}
+    for _, p in model.named_parameters():
+        dtype = p.dtype
+        if dtype not in dtypes: dtypes[dtype] = 0
+        dtypes[dtype] += p.numel()
+    total = 0
+    for k, v in dtypes.items(): total+= v
+    for k, v in dtypes.items():
+        print(k, v, v/total)
+
+    do_train = True
+
+    # Launch training
+    print("Training...")
+
+    if do_train:
+        train_result = trainer.train()
+        metrics = train_result.metrics
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+        print(metrics)
+
+    ###
+
+    # Saving model
+    print("Saving last checkpoint of the model...")
+    os.makedirs(output_dir, exist_ok=True)
+    trainer.model.save_pretrained(output_dir)
+
+    # Free memory for merging weights
+    del model
+    del trainer
+    torch.cuda.empty_cache()
+
+# TODO 这里file_path改成自己的file 确保你的数据在Prompt的column里
+def main(file_path = "", output_dir = "lawllm_checkpoint_gemma"):
+    model_name = "google/gemma-7b"
+    bnb_config = create_bnb_config()
+    model, tokenizer = load_model(model_name, bnb_config)
+    # df = pd.read_csv(file_path)
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    df = pd.DataFrame(data, columns=['Prompt'])
+    dataset = Dataset.from_pandas(df)
+    _preprocessing_function = partial(preprocess_batch, tokenizer=tokenizer)
+
+    dataset = dataset.map(
+            _preprocessing_function,
+            batched=True,
+        )
+    dataset = dataset.shuffle(seed=seed)
+
+    # output_dir = output_dir
+    train(model, tokenizer, dataset, output_dir)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process file path and output directory.")
+    parser.add_argument("--file_path", default="train_file.json", help="Path to the input file")
+    parser.add_argument("--output_dir", default="final_checkpoint", help="Directory for the output")
+
+    args = parser.parse_args()
+
+    main(args.file_path, args.output_dir)
